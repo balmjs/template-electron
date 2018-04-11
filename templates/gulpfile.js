354a1f3c6858a2200c7d884880251e10f6ced44d
elf-mouse <ifmouz@gmail.com>
2018-04-11 10:14:29 +0800

    update balm@0.19.0

--- templates/gulpfile.js
+++ templates/gulpfile.js
@@ -1,3 +1,6 @@
+// Documentation - http://balmjs.com/docs/en/configuration/toc.html
+// 中文文档 - http://balmjs.com/docs/zh-cn/configuration/toc.html
+
 var balm = require('balm');
 var packager = require('electron-packager');
 var packagerConfig = require('./packager.config');
@@ -34,8 +37,6 @@ balm.config = {
     }
   }
   // More Config
-  // Documentation - http://balmjs.com/docs/en/configuration/toc.html
-  // 中文文档 - http://balmjs.com/docs/zh-cn/configuration/toc.html
 };
 
 balm.afterTask = () => {
